75fed289305e38b2bd0559cd6c3c69626be89aa7
BrandonBaLu <1719110177@utectulancingo.edu.mx>
2022-06-19 15:56:58 +0000

    jalo

--- apirest/test/test_main.py
+++ apirest/test/test_main.py
@@ -1,8 +1,9 @@
 from fastapi.testclient import TestClient
 
-from code.main import  app
+from code.main import app
 
 clientes = TestClient(app)
+
 
 
 def test_index():
@@ -24,5 +25,29 @@ def test_clientes():
 def test_clientes():
     response = clientes.get("/clientes/1") #request
     data = [{"id_cliente":1,"nombre":"Brandon","email":"patolucas.bbl@gmail.com"}]
+    assert response.status_code == 200
+    assert response.json() == data
+
+
+
+
+def test_cliente_insert():
+    payload={ "id_cliente": 4, "nombre": "str", "email" : "str"}
+    response = clientes.post("/clientes/", json=payload)  #Solicitud
+    data = {"message":"Cliente insertado"}
+    assert response.status_code ==200
+    assert response.json()==data
+
+def test_cliente_update():
+    payload={ "id_cliente": 4, "nombre": "Cliente actualizado", "email" : "actualizado@email.com"}
+    response = clientes.put("/clientes/", json=payload)  #Solicitud
+    data = {"message":"Cliente actualizado"}
+    assert response.status_code ==200
+    assert response.json()==data
+    
+def test_delete_clientes():
+    payload={ "id_cliente": 4, "nombre": "Cliente actualizado", "email" : "actualizado@email.com"}
+    response = clientes.delete("/clientes/4", json=payload)
+    data = {"message": "Cliente eliminado"}
     assert response.status_code == 200
     assert response.json() == data
